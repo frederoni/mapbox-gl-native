6264a14349157d4786783b3b5f4f6c99ddd60f7e
Tobrun <tobrun@mapbox.com>
2016-05-09 11:17:52 +0200

    [android] #3276 - remove Marker View implementation

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/MapboxMap.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/MapboxMap.java
@@ -624,6 +624,8 @@ public class MapboxMap {
     void setViewMarkersBoundsTaskResult(MapView.MarkerInBoundsTask.Result result) {
         Map<Marker, View> outBoundsMarker = result.getOutBounds();
         View convertView;
+
+        // out of bounds markers
         for (Map.Entry<Marker, View> outBoundsEntry : outBoundsMarker.entrySet()) {
             convertView = outBoundsEntry.getValue();
             if (convertView != null) {
@@ -633,6 +635,7 @@ public class MapboxMap {
             }
         }
 
+        // in bounds markers
         List<Marker> inBoundsMarkers = result.getInBounds();
         for (final Marker marker : inBoundsMarkers) {
             convertView = viewSimplePool.acquire();
@@ -938,11 +941,22 @@ public class MapboxMap {
     @UiThread
     public void removeAnnotation(@NonNull Annotation annotation) {
         if (annotation instanceof Marker) {
-            ((Marker) annotation).hideInfoWindow();
+            Marker marker = (Marker) annotation;
+            marker.hideInfoWindow();
+            removeMarkerView(annotation.getId());
         }
         long id = annotation.getId();
         mMapView.removeAnnotation(id);
         mAnnotations.remove(id);
+    }
+
+    private void removeMarkerView(long id){
+        View viewHolder = mMarkerViews.get(id);
+        if(viewHolder!=null) {
+            viewHolder.setVisibility(View.GONE);
+            viewSimplePool.release(viewHolder);
+        }
+        mMarkerViews.remove(id);
     }
 
     /**
@@ -969,6 +983,7 @@ public class MapboxMap {
             Annotation annotation = annotationList.get(i);
             if (annotation instanceof Marker) {
                 ((Marker) annotation).hideInfoWindow();
+                removeMarkerView(annotation.getId());
             }
             ids[i] = annotationList.get(i).getId();
         }
